23d8634a1f71665abfc7cf7aaae02f40059d05ad
Pawan Kumar <pk.soft29@gmail.com>
2020-11-08 19:00:44 +0530

    Added : function to check maximum of float

--- main/java/FindMaximum.java
+++ main/java/FindMaximum.java
@@ -9,11 +9,26 @@ public class FindMaximum {
         {
             max = sValue;
         }
-        if(tValue.compareTo(max) >0)
+        if(tValue.compareTo(max) > 0)
         {
             max = tValue;
         }
 
         return max;
+    }
+
+    public float maximumFloatValue(Float value1, Float value2, Float value3) {
+        Float max = value1;
+
+        if(value1.compareTo(max) > 0)
+        {
+            max = value1;
+        }
+        if(value2.compareTo(max) >0)
+        {
+            max = value3;
+        }
+        return max;
+
     }
 }
--- test/java/TestMaximumValueClass.java
+++ test/java/TestMaximumValueClass.java
@@ -30,4 +30,35 @@ class TestMaximumValue {
         int max = findMaximum.maximumValue(9, 7, 8);
         Assert.assertNotEquals(7, max);
     }
+
+    @Test
+    public void testMaximum_ProvideFirstFlotValueMaximum_ShouldReturnFirstValue() {
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.maximumFloatValue(20.2f, 9.1f, 8.7f);
+        Assert.assertEquals(20.2f, max, 0.00002);
+    }
+
+    @Test
+    public void testMaximum_ProvideSecondFlotValueMaximum_ShouldReturnSecondValue() {
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.maximumFloatValue(9.1f, 20.2f, 8.7f);
+        Assert.assertEquals(20.2f, max, 0.00002);
+    }
+
+    @Test
+    public void testMaximum_ProvideThirdFlotValueMaximum_ShouldReturnThirdValue() {
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.maximumFloatValue(20.2f, 9.1f, 21.7f);
+        Assert.assertEquals(21.7f, max, 0.00002);
+    }
+
+    @Test
+    public void provideFloatValue_passWrongMaximum_shouldReturnFalse() {
+        FindMaximum maximum = new FindMaximum();
+        float max = maximum.maximumFloatValue(33.3f, 27.7f, 7.7f);
+        Assert.assertNotEquals(27.7f, max, 0.00003);
+    }
+
+
+
 }
